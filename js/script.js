7220ac2111c342e0dd96bcd46e42dab4ec0e028f
DudaDias <dudadias.1783@gmail.com>
2021-10-22 13:02:32 -0300

    Calculo de aplicacoes arquivo img. arquivo css. arquivo javaScript.
    Signed-off-by: DudaDias <dudadias.1783@gmail.com>

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function ipva(){
+
+   const valBank = 0.0052;
+   let aplicar = prompt("Informe o valor que irá aplicar:");
+   console.log(aplicar);
+
+   let mes = prompt("Informe quantos meses deixará aplicado:");
+   console.log(mes);
+
+   let calcMes = valBank*mes;
+   console.log(calcMes);
+
+   let resultFInal = aplicar * calcMes;
+   alert(`Seus ${aplicar} ao final de ${mes} meses renderão ${resultFInal.toFixed(3)} de IPVA`);
+
+}
